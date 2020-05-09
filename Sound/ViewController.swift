38ca6cbeff445c8b131007782a50ceee473e8a85
Marin Ueda <marin96nym@gmail.com>
2020-05-09 19:24:53 +0900

    Initial Commit

---
+++ Sound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sound
+//
+//  Created by 植田真梨 on 2020/05/09.
+//  Copyright © 2020 Ueda Maririn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
