0e55114d044e5a1379eeae057f9bcfbb5ea7b2dc
Marin Ueda <marin96nym@gmail.com>
2020-05-14 04:11:21 +0900

    応用編完成！

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -13,8 +13,16 @@ class ViewController: UIViewController {
  
  
     @IBOutlet var drumButton: UIButton!
+    
+    @IBOutlet var pianoButton: UIButton!
+    
+    @IBOutlet var guitarButton: UIButton!
    //ドラムのサウンドファイルを読み込んでプレイヤーを作る
     let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
+    
+    let pianoSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "pianoSound")!.data)
+    
+    let guitarSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "guitarSound")!.data)
     
     
     
@@ -37,7 +45,25 @@ class ViewController: UIViewController {
     drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
     }
     
+   @IBAction func touchDownPianoButton(){
+        
+    pianoButton.setImage(UIImage(named: "pianoPlayingImage"), for: .normal)
+        
+        pianoSoundPlayer.currentTime = 0
+        
+        pianoSoundPlayer.play()
+    }
+    @IBAction func touchUpPianoButton(){
+        pianoButton.setImage(UIImage(named: "pianoImage"), for: .normal )
+    }
     
-
+    @IBAction func touchDownGuitarButton(){
+           guitarButton.setImage(UIImage(named: "guitarPlayingImage"), for: .normal)
+           guitarSoundPlayer.currentTime = 0
+           guitarSoundPlayer.play()
+       }
+    @IBAction func touchUpGuitarButton(){
+           guitarButton.setImage(UIImage(named: "guitarImage"), for: .normal )
+       }
 }
 
